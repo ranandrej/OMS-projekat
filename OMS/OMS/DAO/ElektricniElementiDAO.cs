21ea20000119ba95bc24ae00b5b6671ddb268d4d
belegiski <belegiski.stevan003@gmail.com>
2023-12-21 01:22:24 +0100

    Elektricni elementi

---
+++ OMS/OMS/DAO/ElektricniElementiDAO.cs
@@ -0,0 +1,69 @@
+﻿using OMS.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OMS.DAO
+{
+    public class ElektricniElementiDAO
+    {
+        public ElektricniElementiDAO() { }
+
+        public List<ElektricniElementi> PronadjiElemente()
+        {
+            List<ElektricniElementi> elementi = new List<ElektricniElementi>();
+            DataBase db = new DataBase();
+            string query = "select * from ElektricniElementi";
+            SQLiteCommand command = db.connection.CreateCommand();
+            command.CommandText = query;
+            db.OpenConnection();
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                ElektricniElementi ee = new ElektricniElementi(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4));
+                elementi.Add(ee);
+            }
+            db.CloseConnection();
+            return elementi;
+        }
+
+        public void UnesiElektricniElement()
+        {
+            ElektricniElementi ee = new ElektricniElementi();
+
+
+
+            Console.WriteLine("Unesite id elektricnog elementa:");
+            ee.IdEl = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Unesite naziv elementa:");
+            ee.NazivEl = Console.ReadLine();
+            Console.WriteLine("Unesite tip elementa:");
+            ee.TipEl = Console.ReadLine();
+            Console.WriteLine("Unesite geografsku lokaciju elementa:");
+            ee.GeoLokEl = Console.ReadLine();
+            Console.WriteLine("Unesite naponski nivo elementa:");
+            ee.NapNivoEl = Console.ReadLine();
+
+            DataBase db = new DataBase();
+            string query = "insert into ElektricniElementi values(@IdEl,@NazivEl,@TipEl,@GeoLokEl,@NapNivoEl,@IDKV)";//unos u tabelu
+            SQLiteCommand command = new SQLiteCommand(query, db.connection);//Kreiranje SQL komande
+            command.Parameters.AddWithValue("@IdEl", ee.IdEl);//Postavljanje vrednosti parametara
+            command.Parameters.AddWithValue("@NazivEl", ee.NazivEl);
+            command.Parameters.AddWithValue("@TipEl", ee.TipEl);
+            command.Parameters.AddWithValue("@GeoLokEl", ee.GeoLokEl);
+            command.Parameters.AddWithValue("@NapNivoEl", ee.NapNivoEl);
+            command.Parameters.AddWithValue("@IDKV", "20020109103025_22");
+            db.OpenConnection();
+
+            if (command.ExecuteNonQuery() > 0)
+            {
+                Console.WriteLine("Elektricni element je uspesno dodat.");
+            };
+            db.CloseConnection();
+        }
+    }
+}
--- OMS/OMS/Program.cs
+++ OMS/OMS/Program.cs
@@ -11,33 +11,43 @@ namespace OMS
 {
     class Program
     {
-        //Pozivanje Metoda
+        //Pozivanje Metoda za Kvar klasu
         static void Main(string[] args)
         {
             KvarDAO kvarDAO = new KvarDAO();
             KvarIspis kvisp = new KvarIspis();
+            ElektricniElementiDAO elementiDao = new ElektricniElementiDAO();
+            ElektricniElementiIspis eeisp = new ElektricniElementiIspis();
             string answer;
-            
-                Console.WriteLine("Izaberite opciju:");
-                Console.WriteLine("1-Unos Kvara");
-                Console.WriteLine("2-Prikaz svih");
-                Console.WriteLine("3-Izlaz");
-                
-                answer = Console.ReadLine();
-            
-           
-                switch (answer)
-                {
-                    case "1":
-                        kvarDAO.UnesiKvar();
-                        break;
-                    case "2":
-                        kvisp.IspisiKvarove();
-                        break;
-                    case "3":
-                        break;
-                }
-            
+
+            Console.WriteLine("Izaberite opciju:");
+            Console.WriteLine("1-Unos Kvara");
+            Console.WriteLine("2-Prikaz svih kvarova");
+            Console.WriteLine("3-Unos elektricnog elementa");
+            Console.WriteLine("4-Prikaz svih elektricnih elemenata");
+            Console.WriteLine("5-Izlaz");
+
+            answer = Console.ReadLine();
+
+
+            switch (answer)
+            {
+                case "1":
+                    kvarDAO.UnesiKvar();
+                    break;
+                case "2":
+                    kvisp.IspisiKvarove();
+                    break;
+                case "3":
+                    elementiDao.UnesiElektricniElement();
+                    break;
+                case "4":
+                    eeisp.IspisiElemente();
+                    break;
+                case "5":
+                    break;
+            }
+
         }
     }
 }
